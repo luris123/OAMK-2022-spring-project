71cdef44c6da8aad2149ef673ca89d1c45d2921a
Anssi Rauhala <anssi.rauhala1@gmail.com>
2022-04-13 10:04:51 +0300

    poistettu turhat tiedostot

--- api/models/database.js
+++
@@ -1,8 +0,0 @@
-const mysql = require('mysql');
-const connection = mysql.createPool({
-  host: 'i54jns50s3z6gbjt.chr7pe7iynqr.eu-west-1.rds.amazonaws.com',
-  user: 'nruoa1u7gp4j1xjs',
-  password: 'clsjcpxzksowk0cc',
-  database: 'f4wbmdmg31nr0iri'
-});
-module.exports = connection;
